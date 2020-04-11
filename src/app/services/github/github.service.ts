608e9208919c1e4a1ff149abc48dc313491c7e8e
Lenny Makori <makorilenny28@gmail.com>
2020-04-11 10:16:40 +0300

    create a github service and include a getUser method

--- src/app/services/github/github.service.ts
+++ src/app/services/github/github.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
 
-  constructor() { }
+  url: string = 'https://api.github.com'
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]>{
+    return this.http.get<User[]>(`${this.url}/users`)
+  }
+
 }
