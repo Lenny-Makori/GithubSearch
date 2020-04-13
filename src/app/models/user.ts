9d4ae1ea0b16cf4c24fe6d2be044849d59e46bde
Lenny Makori <makorilenny28@gmail.com>
2020-04-13 14:28:39 +0300

    create a fetchUser method in the service

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,3 +1,3 @@
 export class User {
-    constructor(public login: string, public avatar_url: string, public bio: string, public public_repos: string, public followers: string, public following: string){}
+    constructor(public login: string, public avatar_url: string, public bio: string, public public_repos: number, public followers: number, public following: number){}
 }
--- src/app/services/github/github.service.ts
+++ src/app/services/github/github.service.ts
@@ -2,20 +2,64 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/models/user';
+import { Repo } from 'src/app/models/repo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
 
+  user: User;
+  repo: Repo;
+  username: string;
+
   apiKey: string = 'b4c6f893e48edf22ce0f8964e559702b07fa1b7a';
 
   url: string = 'https://api.github.com'
 
-  constructor(private http: HttpClient) { }
-
-  getUsers(): Observable<User[]>{
-    return this.http.get<User[]>(`${this.url}/users?+${this.apiKey}`)
+  constructor(private http: HttpClient) { 
+    this.user = new User("", "", "", 0, 0, 0);
+    this.repo = new Repo("", "");
+    this.username = 'Lenny-Makori' 
   }
+
+  getUser(username: string){
+    this.username = username;
+  }
+
+  fetchUser(){
+    interface UserApiResponse{
+      login: string;
+      avatar_url: string;
+      bio: string;
+      public_repos: number;
+      followers: number;
+      following: number;
+    }
+
+    const promise = new Promise((resolve,reject) => {
+      this.http.get<UserApiResponse>(`${this.url}/users/${this.username}?access_token=${this.apiKey}`)
+      .toPromise().then(response => {
+        this.user.login = response.login;
+        this.user.avatar_url = response.avatar_url;
+        this.user.bio = response.bio;
+        this.user.public_repos = response.public_repos;
+        this.user.followers = response.followers;
+        this.user.following = response.following;
+        
+        resolve();
+      },
+      error =>{
+        reject(error);
+      })
+    })
+    return promise;
+  }
+
+  
+
+  
+
+  
 
 }
